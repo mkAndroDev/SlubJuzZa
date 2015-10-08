d360f79032916c9dcf3820681e904ee47ed13332
Maciej Krawczyk <mkandrodev@gmail.com>
2015-10-08 18:11:51 +0200

    Added layout and activity for Menu and about

--- app/src/main/java/com/krawczyk/maciej/slubjuzza/MainActivity.java
+++ app/src/main/java/com/krawczyk/maciej/slubjuzza/MainActivity.java
@@ -1,5 +1,6 @@
 package com.krawczyk.maciej.slubjuzza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,20 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent startAbout = new Intent(this, AboutActivity.class);
+            startActivity(startAbout);
             return true;
         }
 
